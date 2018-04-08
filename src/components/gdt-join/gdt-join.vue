47e29276540803fa4e257b3066dd8618a3b11292
Xaviju <xaviju@gmail.com>
2018-04-08 19:42:27 +0200

    Join

--- src/components/gdt-join/gdt-join.vue
+++ src/components/gdt-join/gdt-join.vue
@@ -8,7 +8,14 @@ export default {
   name: 'gdt-join',
   data() {
     return {
-      join: '',
+      join: null,
+      loading: null,
+      errors: [],
+      name: null,
+      email: null,
+      userPayment: 'cuota-general',
+      telephone: null,
+      message: null,
     };
   },
   created() {
@@ -21,15 +28,38 @@ export default {
   methods: {
     getJoin() {
       const url = 'http://guindostan.org/wp-json/wp/v2/pages/24';
+      this.loading = true;
 
       return window.fetch(url)
         .then(response => response.json())
         .then((json) => {
+          this.loading = false;
           this.join = json;
         })
         .catch((err) => {
           throw err.message;
         });
+    },
+    checkForm(event) {
+      event.preventDefault();
+      const formURL = 'http://guindostan.org/wp-json/contact-form-7/v1/contact-forms/754/feedback';
+      const formData = new FormData();
+      formData.append('user-name', this.name);
+      formData.append('user-email', this.email);
+      formData.append('user-payment', this.userPayment);
+      formData.append('user-phone', this.telephone);
+      formData.append('user-message', this.message);
+      return window.fetch(formURL, {
+        method: 'POST',
+        body: formData,
+      })
+      .then(response => response.json())
+      .then((json) => {
+        this.join = json;
+      })
+      .catch((err) => {
+        throw err.message;
+      });
     },
   },
 };
